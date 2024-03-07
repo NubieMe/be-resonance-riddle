32ca8d499166766a610c7acd00c965e128ced871
NubieMe <fahmi.mellow@gmail.com>
2024-03-07 17:37:00 +0700

    feat: cloudinary added

---
+++ app/Http/Middleware/Cloudinary.php
@@ -0,0 +1,10 @@
+<?php
+use Cloudinary\Configuration\Configuration;
+          
+Configuration::instance([
+  'cloud' => [
+    'cloud_name' => 'dydmnzkfh', 
+    'api_key' => '268628729926561', 
+    'api_secret' => 'kx4RyB2lNTOvaDMCTCDRKsPbqus'],
+  'url' => [
+    'secure' => true]]);
